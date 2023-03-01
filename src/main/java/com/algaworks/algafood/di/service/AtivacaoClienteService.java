41dfff316b0e51f56042148f5bdfced66e7408be
hbakergo <hbakergoveia@hotmail.com>
2023-03-01 09:28:06 -0300

    Implementando tipos de ponto de injeção com autowired

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,20 +1,32 @@
 package com.algaworks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 	
+	@Autowired
 	private Notificador notificador;
 	
+	/*@Autowired
 	public AtivacaoClienteService(Notificador notificador) {
 		this.notificador = notificador;
-	}
+	}*/
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
+
+	/*public void setNotificador(Notificador notificador) {
+		this.notificador = notificador;
+	}*/
+	
+	
 
 }
